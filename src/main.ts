d85066f5a3f17ea14a4943fcaf9e3bfc52c296b2
Kabira K <kabira@monallabs.in>
2024-10-29 12:37:03 +0000

    using mount

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import './app.postcss';
 import App from './App.svelte';
 import { justpyComponents, setJustpyComponents } from './store_jpcomponents';
+import { mount } from 'svelte';
 
 export {App,  setJustpyComponents};
+export function mountApp(target) {
+       return mount(App, { target: target });
+}
